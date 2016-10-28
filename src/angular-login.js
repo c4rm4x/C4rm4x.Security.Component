5008e951e27a3dd40d34865093cac1aae4e98d38
carmelo@purusdigital.com <carmelo@purusdigital.com>
2016-10-28 14:39:20 +0100

    Adapting directive to look more like FB'

--- src/angular-login.js
+++ src/angular-login.js
@@ -17,7 +17,7 @@ fbLoginDirectives.directive('fbLoginWelcomeUser', ['$window', 'fbLoginConfig', '
 					FB.login();
 				};
 			},		
-			template: '<div ng-hide=\'auth.isLoggedIn()\'><button ng-click=\'onlogin()\' class=\'loginBtn loginBtn--facebook\'>Login with Facebook</button></div><div ng-show=\'auth.isLoggedIn()\'><div><img ng-src=\'{{auth.getPicture()}}\'><span>{{auth.getUsername()}}</span></div> (<a href=\'javascript:void(0)\' ng-click=\'onlogout()\'>Logout</a>)</div>'
+			template: '<div ng-hide=\'auth.isLoggedIn()\'><button ng-click=\'onlogin()\' class=\'loginBtn loginBtn--facebook\'>Login with Facebook</button></div><div ng-show=\'auth.isLoggedIn()\'><img width=\'25px\' height=\'25px\' ng-src=\'{{auth.getPicture()}}\'><span style=\'margin: 5px; font-size: 12px; font-weight: bold; color: white;\'>{{auth.getUsername()}}</span><div class=\'btn-group\'><button type=\'button\' class=\'btn btn-xs btn-primary dropdown-toggle dropdown-toggle-split\' data-toggle=\'dropdown\' aria-haspopup=\'true\' aria-expanded=\'false\'><span class=\'caret\'></span></button><ul class=\'dropdown-menu\'><li><a class=\'dropdown-item\' href=\'#\' ng-click=\'onlogout()\'>Log out</a></li></ul></div></div></div>'
 		};
 	}]);
 'use strict';
